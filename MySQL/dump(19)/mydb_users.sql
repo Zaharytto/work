f876fbca8e56ee31b9d1582897d39b2504bf03a4
Zaharytto <zaxar.kravchenko.96@mail.ru>
2022-10-22 00:19:50 +0300

    19

---
+++ MySQL/dump(19)/mydb_users.sql
@@ -0,0 +1,56 @@
+-- MySQL dump 10.13  Distrib 8.0.31, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: mydb
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `is_online` tinyint(1) NOT NULL,
+  `full_name` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `phone` varchar(30) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `is_notification` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,0,'Петичкин Иван Николаевич','ivan@mail.ru','+7(999) 123-45-01','81dc9bdb52d04dc20036dbd8313ed055',0),(2,0,'Пупкин Алексей Андреевич','alex@mail.ru','+7(999) 123-45-02','9996535e07258a7bbfd8b132435c5962',1),(3,0,'Петрушевич Валерий Иванович','vala@mail.ru','+7(999) 123-45-03','7bccfde7714a1ebadf06c5f4cea752c1',0),(4,1,'Зеньцов Максим Викторович','maxim@mail.ru','+7(999) 123-45-04','a9eb812238f753132652ae09963a05e9',0),(5,0,'Васечкин Тарас Фёдорович','taras@mail.ru','+7(999) 123-45-05','d38901788c533e8286cb6400b40b386d',0),(6,1,'Пономарёв Дмитрий Александрович','dima@mail.ru','+7(999) 123-45-06','b3ba8f1bee1238a2f37603d90b58898d',0),(7,1,'Капустин Артём Олегович','artem@mail.ru','+7(999) 123-45-07','4122cb13c7a474c1976c9706ae36521d',1),(8,0,'Морковкин Роман Викторович','roma@mail.ru','+7(999) 123-45-08','6c14da109e294d1e8155be8aa4b1ce8e',1);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-22  0:10:38
