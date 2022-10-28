06a36aee781d20a7a8bc421dfb7cac192c245bdd
Zaharytto <zaxar.kravchenko.96@mail.ru>
2022-10-28 17:47:17 +0300

    21

--- 21/ClientNotices.php
+++ 21/ClientNotices.php
@@ -39,7 +39,7 @@ class Censor
 {
     public function censor($message):string
     {
-        return substr_replace($message, '***', 0,);
+        return preg_replace('/[^.]/', '*', $message);
     }
 }
 
@@ -54,21 +54,20 @@ class Notification
     
     public function  __construct($notificationChannel)
     {
-        if (in_array ($notificationChannel, Notification::AVAIABLE_CHANNEL)) {
+        if (in_array ($notificationChannel, self::AVAIABLE_CHANNEL)) {
             $this->notificationChannel = $notificationChannel;
             }
     }
 
     public function sendTo($name, $contact, $message)
     {
-        echo "Уведомление клиенту: $name на $this->notificationChannel ($contact): $message";
+        return "Уведомление клиенту: $name на $this->notificationChannel ($contact): $message";
     }
-
 }
 
 
 $petya = new User('Петя', 'petya@gmail.com', +375257845168, 20);
- $dima = new User('Дима', 'dima@mail.ru');
+$dima = new User('Дима', 'dima@mail.ru');
 
 
 function notify(User $user, $message)
@@ -79,13 +78,13 @@ function notify(User $user, $message)
         $censor = new Censor();
         $messageNew = $censor->censor($message);
 
-        if ($user->phone != 0) {
-            $notif = new Notification(Notification::PHONE_CHANNEL);
-            return $notif->sendTo($user->name, $user->phone, $messageNew);
-        } else {
-            $notif = new Notification(Notification::EMAIL_CHANNEL);
-            return $notif->sendTo($user->name, $user->email, $messageNew);
-        }
+        if (!empty($user->email) && $user->phone !== 0) {
+            $notifEmail = new Notification(Notification::EMAIL_CHANNEL);
+            $notifPhone = new Notification(Notification::PHONE_CHANNEL);
+            return $notifPhone->sendTo($user->name, $user->phone, $messageNew) . $notifEmail->sendTo($user->name, $user->email, $messageNew);
+        }             
+        $notif = new Notification(Notification::EMAIL_CHANNEL);
+        return$notif->sendTo($user->name, $user->email, $messageNew);
     }
 }
 
@@ -93,4 +92,3 @@ $messageFromPetya = 'Привет, как дела?';
 $messageFromDima = 'Привет, когда приедешь в гости?';
 var_dump(notify($petya, $messageFromPetya));
 var_dump(notify($dima, $messageFromDima));
-
