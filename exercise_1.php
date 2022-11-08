7b4bce0dbdbc54466f39a9019b1e81565ee10cd1
Zaharytto <zaxar.kravchenko.96@mail.ru>
2022-11-08 20:20:31 +0300

    tz

--- exercise_1.php
+++ exercise_1.php
@@ -5,16 +5,16 @@ class Database
     public $id;
     public $name;
     public $surname;
-    public static $dateOfBirth;
+    public static $birthday;
     public static $gender;
-    public $cityOfBirth;
+    public $birthplace;
 
     public function addData()
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=mydb', 'root', 'root');
-        $result = $pdo->prepare("INSERT INTO 'users' (id, name, surname, date_of_birth, gender, city_of_birth) 
-        values (:id, :name, :surname, :date_of_birth, :gender, :city_of_birth)");
-        $result->execute([':id' => $id, ':name' => $name, ':surname' => $surname, ':date_of_birth' => $dateOfBirth, ':gender' => $gender, ':city_of_birth' => $cityOfBirth]);
+        $pdo = new PDO('mysql:host=localhost;dbname=mydb', 'root', 'root');  
+        $result = $pdo->prepare("INSERT INTO `users` (id, name, surname, birthday, gender, birthplace)
+        values (:id, :name, :surname, :birthday, :gender, :birthplace)");
+        $result->execute([':id' => $this->id, ':name' => $this->name, ':surname' => $this->surname, ':birthday' => $this->birthday, ':gender' => $this->gender, ':birthplace' => $this->birthplace]);
         $pdo = null;
         return $result;
     }
@@ -22,15 +22,15 @@ class Database
     public function deleteData()
     {
         $pdo = new PDO('mysql:host=localhost;dbname=mydb', 'root', 'root');
-        $result = $pdo->prepare("DELETE FROM 'users' WHERE id = :id");
-        $result->execute([':id' => $id]);
+        $result = $pdo->prepare("DELETE FROM `users` WHERE id = :id");
+        $result->execute([':id' => $this->id]);
         $pdo = null;
         return $result;
     }
 
-    public static function getAge($dateOfBirth)
+    public static function getAge($birthday)
     {
-        $diff = date( 'Ymd' ) - date('Ymd', strtotime($dateOfBirth));   
+        $diff = date( 'Ymd' ) - date('Ymd', strtotime($birthday));   
         return substr( $diff, 0, -4 );
     }
 
@@ -39,25 +39,25 @@ class Database
      return $gender ? 'male' : 'female';
     }
 
-    public function __construct($id, $name, $surname, $dateOfBirth, $gender, $cityOfBirth)
+    public function __construct($id, $name, $surname, $birthday, $gender, $birthplace)
     {
         $this->id = $id;
         $this->name = $name;
         $this->surname = $surname;
-        $this->dateOfBirth = $dateOfBirth;
+        $this->birthday = $birthday;
         $this->gender = $gender;
-        $this->cityOfBirth = $cityOfBirth;
+        $this->birthplace = $birthplace;
 
-        if (empty($id)) {
-            return $this->addData();
-        } else {
-            if (find(int $id)) {                                                         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                $pdo = new PDO('mysql:host=localhost;dbname=mydb', 'root', 'root');
-                $result = $pdo->prepare("SELECT * FROM 'users' WHERE id = :id");
-                $result->execute([':id' => $id]);
-                $pdo = null;
-            }
-        }
+        // if (empty($id)) {
+        //     return $this->addData();
+        // } else {
+        //     if (find(int $id)) {                                                         //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+        //         $pdo = new PDO('mysql:host=localhost;dbname=mydb', 'root', 'root');
+        //         $result = $pdo->prepare("SELECT * FROM 'users' WHERE id = :id");
+        //         $result->execute([':id' => $id]);
+        //         $pdo = null;
+        //     }
+        // }
     }
 
     public function returnFormatted()
@@ -66,11 +66,21 @@ class Database
         $class->id = $this->id;
         $class->name = $this->name;
         $class->surname = $this->surname;
-        $class->birthday = self::getAge($this->dateOfBirth);
+        $class->birthday = self::getAge($this->birthday);
         $class->gender = self::convert($this->gender);
-        $class->cityOfBirth = $this->cityOfBirth;
+        $class->birthplace = $this->birthplace;
         return $class;        
     }
 }
+
+
+
+$x = new Database(1, 'Pet', 'Petov', '1995-09-10', 1, 'London');
+
+
+var_dump(Database::getAge('1995-09-10'));
+
+var_dump(Database::convert(0));
+
 
 ?>
